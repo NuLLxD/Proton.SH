79f54dd95c5b54ea94c5c9fe10976e8c53625eca
Chris Douglas <35948442+chrisgdouglas@users.noreply.github.com>
2025-02-02 20:44:32 -0600

    v1.1 - If the user supplies an AppId, just launch CE, otherwise go into interactive mode

--- cehelper.sh
+++ cehelper.sh
@@ -6,29 +6,32 @@
 # Use the AppId to get the correct prefix to link CE
 # Specify an AppID to launch CE, or use the automatic option to launch in the current prefix - this will fail if the CE executable is not present.
 
-PS3='Select choice: '
-select choice in "Auto Launch Cheat Engine in active prefix
-" "Launch Cheat Engine in specified prefix
-" "Get AppId of Running Game
-"; do
-    case $REPLY in
-        [1-3]) break ;;
-            *) echo 'Try again' >&2
-    esac
-done
+if [ $# = 0 ]; then
+    PS3='Select choice: '
+    select choice in "Auto Launch Cheat Engine in active prefix
+    " "Get AppId of Running Game
+    "; do
+        case $REPLY in
+            [1-2]) break ;;
+                *) echo 'Try again' >&2
+        esac
+    done
+else
+    REPLY=1
+fi
 
 # Get the executable path of all running processes
 # 'grep' for AppId, include the first 20 characters after match
-# 'cut' the "AppId=" match from the result, return only those that match the '=' delimiter
+# 'cut' the "AppId=" match fro the result, return only those that match the '=' delimiter
 # 'grep' for 1-20 numbers, return only the first match
 APPID=`ps -ef | grep -oP "AppId.{0,20}" | cut -sf2- -d= | grep -Eom1 "[0-9]{0,20}"`
 
-if [[ $REPLY -eq 1 || $REPLY -eq 2 ]]; then
+if [ $REPLY -eq 1 ]; then
     # Runs Cheat Engine in the specified prefix.
     # Note, you must have CE extracted to your system
     # CE executable seems to be required within the game's prefix
     # I use a symbolic link within the prefix. Example:
-    # ln -s [PATH TO PREFIX]/drive_c/CheatEngine /home/[USER]/.local/bin/CheatEngine/
+    # ln -s /home/user/.local/bin/CheatEngine/ [PATH TO PREFIX]/drive_c/CheatEngine
 
     if [ $# = 0 ]; then
         # Use Automatic option, should be the current running prefix
