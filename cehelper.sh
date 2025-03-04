5a8a68f2b898bc3130d9bbe202ce68afbf73f405
Chris Douglas <35948442+chrisgdouglas@users.noreply.github.com>
2025-03-03 20:52:22 -0600

    Simplified APPID acquistion
    - use awk instead of grep for getting the final AppID
    - Echo the AppId properly

--- cehelper.sh
+++ cehelper.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # Cheat Engine Helper by Chris Douglas
 # Launch CE in the specified prefix, or get the AppId for a running game
@@ -24,14 +24,14 @@ fi
 # 'grep' for AppId, include the first 20 characters after match
 # 'cut' the "AppId=" match from the result, return only those that match the '=' delimiter
 # 'grep' for 1-20 numbers, return only the first match
-APPID=`ps -ef | grep -oP "AppId.{0,20}" | cut -sf2- -d= | grep -Eom1 "[0-9]{0,20}"`
+APPID=`ps -ef | grep -oP "AppId.{0,20}" | cut -sf2- -d= | awk '{print $1}'`
 
 if [ $REPLY -eq 1 ]; then
     # Runs Cheat Engine in the specified prefix.
     # Note, you must have CE extracted to your system
     # CE executable seems to be required within the game's prefix
     # I use a symbolic link within the prefix. Example:
-    # ln -s [PATH TO PREFIX]/drive_c/CheatEngine /home/[USER]/.local/bin/CheatEngine/
+    # ln -s /home/[USER]/.local/bin/CheatEngine/ [PATH TO PREFIX]/drive_c/CheatEngine
 
     if [ $# = 0 ]; then
         # Use Automatic option, should be the current running prefix
@@ -49,5 +49,5 @@ if [ $REPLY -eq 1 ]; then
 
     STEAM_COMPAT_DATA_PATH="$COMPDATA/$GAME/" STEAM_COMPAT_CLIENT_INSTALL_PATH="$STEAMPATH" "$PRTEXEC" run "$CEPATH/cheatengine-x86_64.exe" </dev/null &>/dev/null &
 else
- echo "$GAME"
+ echo "$APPID"
 fi
